a70d9b907f96a8a1cb98ad53eb4d0d659b76631d
owatu <oaoto1811@gmail.com>
2022-05-28 21:55:09 +0900

    change design
    videodetail table: de<table>tag
    etc

--- pages/api/video.ts
+++ pages/api/video.ts
@@ -9,12 +9,9 @@ export default async function video(req: NextApiRequest, res: NextApiResponse) {
   const watch = await got(`https://www.youtube.com/watch?v=${req.query.v}`)
 
   console.log("watch.status:", watch.statusCode)
-  console.log("watch.body:")
-  console.log(watch.body)
 
   const match = watch.body.match(/ytInitialPlayerResponse\s*=\s*(\{.*?\});/)
   if (!match?.[1]) {
-    res.setHeader("Access-Control-Allow-Origin", "*")
     res.status(500)
     res.send(`not found match: ${match}`)
     return
