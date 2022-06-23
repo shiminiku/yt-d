23d7ffb7bc1e0aa58871ff80c4d0fbd9077d6faa
owatu <oaoto1811@gmail.com>
2022-06-23 16:56:06 +0900

    update yt-o

--- pages/api/getsig.ts
+++ pages/api/getsig.ts
@@ -1,13 +1,16 @@
 import type { NextApiRequest, NextApiResponse } from "next"
 import got from "got"
 import logAPIAccess from "../../lib/apiLogger"
-import { getURL } from "youtube-otosuyatu"
+import { getVideoURL } from "youtube-otosuyatu"
 
 export default async function getsig(req: NextApiRequest, res: NextApiResponse) {
   logAPIAccess(req)
 
   const watch = await got(`https://www.youtube.com/watch?v=${req.query.v}`)
-  const url = await getURL(req.query.sc, `https://www.youtube.com${watch.body.match(/script src="(.*?base.js)"/)[1]}`)
+  const url = await getVideoURL(
+    req.query.sc as string,
+    `https://www.youtube.com${watch.body.match(/script src="(.*?base.js)"/)[1]}`
+  )
 
   res.setHeader("Access-Control-Allow-Origin", "*")
   res.setHeader("Content-Type", "text/plain")
