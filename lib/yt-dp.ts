0e3868b2df4304ff324687abe768982b1696c32f
shiminiku <34957270+shiminiku@users.noreply.github.com>
2024-08-16 14:41:08 +0900

    TODO: poToken

--- lib/yt-dp.ts
+++ lib/yt-dp.ts
@@ -1,5 +1,6 @@
 const BASE_URL = "https://yt-dp.smnk.workers.dev"
 
+// TODO: What the poToken???
 export async function GET_playerResponse(videoId: string) {
   return (await fetch(`${BASE_URL}/playerResponse?v=${videoId}`).then((r) =>
     r.status === 200 ? r.json() : Promise.reject(new Error("returned not 200."))
